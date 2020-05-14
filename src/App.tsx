5304953feca8f9a6e427f59088953eeb0f0357ad
Elias Rut <eli4srut@gmail.com>
2020-05-14 18:59:54 +0200

    Initial push.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Header } from 'semantic-ui-react';
 import './App.css';
 
-function App() {
-  return (
+export interface AppProps {
+  headerText: string;
+}
+
+export const App: React.FunctionComponent<AppProps> = (props) => {  return (
     <div className="App">
+      <Header>{props.headerText}</Header>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App 
+      headerText="Coding Session 5"
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
